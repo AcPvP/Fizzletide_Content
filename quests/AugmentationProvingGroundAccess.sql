64db7bde68f3678206d7c59f321bd9a33c70cf16
AcPvP <89650671+AcPvP@users.noreply.github.com>
2021-09-20 15:23:53 -0400

    Reorganize content folder / fiz stipends timer and cost / fix T8 loot for Hidden Forest

--- quests/AugmentationProvingGroundAccess.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'AugmentationProvingGroundAccess';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('AugmentationProvingGroundAccess', 2, -1, 'Flagged to Enter Augmentation Realm', '2021-01-21 06:51:50');
